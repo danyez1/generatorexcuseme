2df8abd8fb0f54e81db9da505b1bb0ac3ff28482
danyez1 <cdanielyanez144@gmail.com>
2022-02-19 01:44:44 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.body.innerHTML =
+    "<h1> OMG! you will not belive but...</h1>" +
+    "<p>" +
+    who[getRandomInt(4)] +
+    " " +
+    action[getRandomInt(4)] +
+    " " +
+    what[getRandomInt(3)] +
+    " " +
+    when[getRandomInt(5)] +
+    "</p>";
 };
